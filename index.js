bdd58fe159ae7a964f100d70e684d03c9efa619a
FGAT3 <feliciengamelin@yahoo.fr>
2024-03-19 11:16:11 +0100

    node-test

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Alice'));
+console.log(chalk.red('Adrien'));
+console.log(chalk.whiteBright('Franck'));
+console.log(chalk.yellowBright('Pierre-Jean'));
